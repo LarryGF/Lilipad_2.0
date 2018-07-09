cab86c379571d1a5326b037c74b4d9f21991dd56
LarryGF <larrywtf609@gmail.com>
2018-07-09 07:18:45 -0400

    modified the tools tab, the dialog bug persists

--- vue/src/components/Tools.vue
+++ vue/src/components/Tools.vue
@@ -1,22 +1,13 @@
 <template>
     <md-content>
-        {{prox}}
-      
-    <md-dialog-prompt
-      :md-active.sync="dialog_active"
-      v-model="amount"
-      md-title="Proxmox"
-      md-input-maxlength="2"
-      md-input-placeholder="Cantidad de instancias de Proxmox en su CD"
-      md-confirm-text="Hecho" />
 
       <md-list>
           <md-list-item>
-              <span>Mapear los componentes de su CD</span>
-              <md-button class="md-primary md-raised" @click="dialog_active=true">Mapear</md-button>
+              <h4>Agregar al programa los elementos de su Centro de Datos a evaluar</h4>
+              <!-- <md-button class="md-primary md-raised" @click="dialog_active=true">Mapear</md-button> -->
           </md-list-item>
           <md-divider></md-divider>
-      <md-list-item v-if="amount">
+      <md-list-item>
         <md-field>
             <label>Usuario de Opennebula</label>
             <md-input v-model="one_user" required></md-input>
@@ -34,24 +25,29 @@
       
       
     </md-list-item> 
-    <md-list-item v-for="i in Number(amount)" :key="i">
+    <md-list-item>
         
         <md-field>
             <label>Usuario de Proxmox</label>
-            <md-input v-model="prox.i.prox_user" required></md-input>
+            <md-input v-model="prox.prox_user" required></md-input>
         </md-field>
       
        <md-field>
             <label>Password de Proxmox</label>
-            <md-input v-model="prox.i.prox_pass" required></md-input>
+            <md-input v-model="prox.prox_pass" required></md-input>
         </md-field>
 
          <md-field>
             <label>Hostname(IP) de Proxmox</label>
-            <md-input v-model="prox.i.prox_ip" required></md-input>
+            <md-input v-model="prox.prox_ip" required></md-input>
         </md-field>
         
     </md-list-item>      
+    <md-list-item>
+        <span>Enviar los datos para ser procesados</span>
+        <md-button class="md-primary md-raised">Enviar</md-button>
+    </md-list-item>
+    <md-divider></md-divider>
       </md-list>
 
     
@@ -75,10 +71,6 @@ export default {
     methods: {
         tool: function(data) {
             eel.tools(data)(result => (this.result = result));
-        },
-        transform: function() {
-            this.amount = Number(this.amount);
-            console.log(this.amount);
         }
     }
 };
